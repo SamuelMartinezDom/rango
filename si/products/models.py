f310611503950dc78461103f4fa7f029f60393a9
Thomas Fernandez <el.fernandito.900@gmail.com>
2022-08-08 15:55:42 -0300

    .

---
+++ si/products/models.py
@@ -0,0 +1,14 @@
+from re import T
+from django.db import models
+from django.forms import CharField
+
+# Create your models here.
+
+class product(models.Model):
+    name = models.CharField(max_length=30)
+    parentesco = models.CharField(max_length=30, null=True, blank=True)
+    age = models.IntegerField()
+    date_of_birth = models.DateTimeField(null=True, blank=True)
+
+def __str__(self):
+    return self.name 
---
+++ si/products/views.py
@@ -0,0 +1,20 @@
+from multiprocessing import context
+from django.shortcuts import render
+from products.models import product
+
+# Create your views here.
+
+def create_product(request):
+    new_product = product.objects.create(
+        name = "Esmeralda",
+        parentesco = "Hermana",
+        age = 6)
+    context = {
+        "new_product": new_product
+    }
+    return render(request, "new_product.html", context=context)
+
+def list(request):
+    products = product.objects.all()
+    context = {"products": products}
+    return render(request, "list.html", context=context)
