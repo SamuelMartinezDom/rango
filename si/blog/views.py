2158aa6a8f0c45f8416e92c182059e6ba5d7398a
Thomas Fernandez <el.fernandito.900@gmail.com>
2022-09-04 21:04:46 -0300

    .

--- si/blog/views.py
+++ si/blog/views.py
@@ -8,16 +8,15 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def create_article(request):
-<<<<<<< HEAD
     if request.user.is_authenticated and request.user.is_superuser:
         if request.method == 'POST':
             form= fomrularios_blog(request.POST)
-=======
+
     if request.method == 'POST':
      form= fomrularios_blog(request.POST, request.FILES)
->>>>>>> 508f93e2263bdf028e8cbe66a9b9a56eeae4b6fa
 
-            if form.is_valid():
+
+    if form.is_valid():
                 article.objects.create(
                 title = form.cleaned_data['title'],
                 body = form.cleaned_data['body'],
@@ -25,7 +24,7 @@ def create_article(request):
                 )     
                 return redirect(articles)
     
-        elif request.method == 'GET':
+    elif request.method == 'GET':
             form_blog = fomrularios_blog
             context = {'form_blog':form_blog}
             return render(request, "articles/new_article.html", context=context)
