701b34e9422f762bc582783796d1fbc81535fc0a
Thomas Fernandez <el.fernandito.900@gmail.com>
2022-09-07 12:53:08 -0300

    falta detail y search

--- si/products/views.py
+++ si/products/views.py
@@ -1,30 +1,30 @@
 from django.shortcuts import render, redirect
 from products.models import Product
 from products.forms import FormularioProducts
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, user_passes_test
 
 
+@user_passes_test(lambda u: u.is_superuser)
 def create_product(request):
     """Esta vista retorna el personaje creado, 
     ademas requiere estar logueado para acceder, de lo contrario te envia al registro."""
-    if request.user.is_authenticated and request.user.is_superuser:
-        if request.method == 'POST':
-            form= FormularioProducts(request.POST, request.FILES)
+    if request.method == 'POST':
+        form= FormularioProducts(request.POST, request.FILES)
 
-            if form.is_valid():
-                Product.objects.create(
-                    name = form.cleaned_data['name'],
-                    category = form.cleaned_data['category'],
-                    description = form.cleaned_data['description'],
-                    price = form.cleaned_data['price'],
-                    stock = form.cleaned_data['stock'],
-                    image= form.cleaned_data["image"])     
-                return redirect(list)
+        if form.is_valid():
+            Product.objects.create(
+                name = form.cleaned_data['name'],
+                category = form.cleaned_data['category'],
+                description = form.cleaned_data['description'],
+                price = form.cleaned_data['price'],
+                stock = form.cleaned_data['stock'],
+                image= form.cleaned_data["image"])     
+            return redirect(list)
 
-        elif request.method == 'GET':
-            form = FormularioProducts
-            context = {'form':form}
-            return render(request, "products/create_product.html", context=context)
+    elif request.method == 'GET':
+        form = FormularioProducts
+        context = {'form':form}
+        return render(request, "products/create_product.html", context=context)
    
 
 @login_required
@@ -44,46 +44,44 @@ def search_products(request):
    return render(request, 'products/search_products.html', context=context)
 
 
+@user_passes_test(lambda u: u.is_superuser)
 def delete_product(request, id):
     """Esta vista retorna un delete del producto que seleccionaste, 
     ademas requiere estar logueado y ser admin para acceder, sino te manda al registro"""
-    if request.user.is_authenticated and request.user.is_superuser:
-        if request.method == 'GET':
-            products = Product.objects.get(id=id)
-            context = {'products':products}
-            return render(request, 'products/delete_product.html',context=context)
-        elif request.method == 'POST':
-            products = Product.objects.get(id=id)
-            products.delete()
-            return redirect(list)
+    if request.method == 'GET':
+        products = Product.objects.get(id=id)
+        context = {'products':products}
+        return render(request, 'products/delete_product.html',context=context)
+    elif request.method == 'POST':
+        products = Product.objects.get(id=id)
+        products.delete()
+        return redirect(list)
 
 
-
+@user_passes_test(lambda u: u.is_superuser)
 def update_product(request, id):
     """Esta vista retorna un update del personaje que seleccionaste, 
     ademas requiere estar logueado y ser admin para acceder, sino te manda al registro"""
-    if request.user.is_authenticated and request.user.is_superuser:
-        if request.method == 'POST':
-            form = FormularioProducts(request.POST)
-            if form.is_valid():
-                products = Product.objects.get(id=id)
-                products.name = form.cleaned_data['name']
-                products.category = form.cleaned_data['category']
-                products.description = form.cleaned_data['description']
-                products.price = form.cleaned_data['price']
-                products.stock = form.cleaned_data['stock']
-                products.save()
-                return redirect(list)
-
-
-        elif request.method == 'GET':
+    if request.method == 'POST':
+        form = FormularioProducts(request.POST)
+        if form.is_valid():
             products = Product.objects.get(id=id)
+            products.name = form.cleaned_data['name']
+            products.category = form.cleaned_data['category']
+            products.description = form.cleaned_data['description']
+            products.price = form.cleaned_data['price']
+            products.stock = form.cleaned_data['stock']
+            products.save()
+            return redirect(list)
 
-            form = FormularioProducts(initial={
-                                        'name':products.name,
-                                        'category':products.category,
-                                        'price':products.price, 
-                                        'description':products.description,
-                                        'stock':products.stock})
-            context = {'form':form}
-            return render(request, 'products/update_product.html', context=context)
+
+    elif request.method == 'GET':
+        products = Product.objects.get(id=id)
+        form = FormularioProducts(initial={
+                                    'name':products.name,
+                                    'category':products.category,
+                                    'price':products.price, 
+                                    'description':products.description,
+                                    'stock':products.stock})
+        context = {'form':form}
+        return render(request, 'products/update_product.html', context=context)
